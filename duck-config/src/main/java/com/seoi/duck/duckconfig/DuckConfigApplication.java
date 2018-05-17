3661214f6a0d0d11dfa5f3a5a9cb3222df4dc9de
Yu Shuai Qi <shuaiqi@ifastfinancial.com.cn>
2018-05-17 20:17:48 +0800

    初始化项目1

---
+++ duck-config/src/main/java/com/seoi/duck/duckconfig/DuckConfigApplication.java
@@ -0,0 +1,12 @@
+package com.seoi.duck.duckconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckConfigApplication.class, args);
+	}
+}
