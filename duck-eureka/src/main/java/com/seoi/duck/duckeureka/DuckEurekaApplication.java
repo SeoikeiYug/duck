604d00ebe89bbc23cb825548a7d47916d1b0d6c0
Yu Shuai Qi <shuaiqi@ifastfinancial.com.cn>
2018-05-22 19:35:27 +0800

    初始化项目2

---
+++ duck-eureka/src/main/java/com/seoi/duck/duckeureka/DuckEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.seoi.duck.duckeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckEurekaApplication.class, args);
+	}
+}
