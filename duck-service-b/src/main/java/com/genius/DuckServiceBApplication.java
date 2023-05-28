e07eeafc03b5e29cb30b4bc65ea2be21136ccea4
seoikei <seoikei@163.com>
2023-05-29 00:57:02 +0800

    initial commit

---
+++ duck-service-b/src/main/java/com/genius/DuckServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.genius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckServiceBApplication.class, args);
+	}
+
+}
---
+++ duck-service-b/src/main/java/com/genius/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.genius.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("index")
+    public String index(String params) {
+        return "B-" + params;
+    }
+
+}
